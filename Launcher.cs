4c66610909139f1ba3d92098bce8dc81b5e09c74
archeryfox <89808784+archeryfox@users.noreply.github.com>
2022-08-13 21:30:39 +0300

    Update Launcher.cs

--- Launcher.cs
+++ Launcher.cs
@@ -12,13 +12,13 @@ namespace Dungeons
         {
             R CodeR = new R();
             V CodeV = new V();
-            Console.WriteLine("\t\tВыберите версию игры:\n\n >Версия Ростика - клавиша R\n >Версия Вити - клавиша V\n");
+            Console.WriteLine("\t\tВыберите версию игры:\n\n >Версия Ростика - клавиша R\n >Версия Вити(Демо) - клавиша V\n");
             Console.WriteLine($"              _\r\n            ,/A\\,\r\n          .//`_`\\\\,\r\n        ,//`____-`\\\\,\r\n      ,//`[_ROVER_]`\\\\,\r\n    ,//`=  ==  __-  _`\\\\,\r\n   //|__=  __- == _  __|\\\\\r\n   ` |  __ .-----.  _  | `\r\n     | - _/       \\-   |\r\n     |__  | .-\"-. | __=|\r\n     |  _=|/)   (\\|    |\r\n     |-__ (/ a a \\) -__|\r\n     |___ /`\\_Y_/`\\____|\r\n          \\)8===8(/");
             ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
             switch (consoleKeyInfo.Key)
             {
-                case ConsoleKey.R: CodeR.Run(); break;
-                case ConsoleKey.V: CodeV.Run(); break;
+                case ConsoleKey.R: Console.Clear(); CodeR.Run(); break;
+                case ConsoleKey.V: Console.Clear(); CodeV.Run(); break;
                 default:
                     Console.Clear();
                     Console.WriteLine("Не та кнопка. Нажмите указанные!");
